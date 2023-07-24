f4709d751ca8c7525fb73f66983beef09629f5de
BARKAVI <61539046+barkavibaru@users.noreply.github.com>
2023-07-24 13:51:16 +0530

    Add files via upload

---
+++ compleate coffee shop project - Copy (2)/project.js
@@ -0,0 +1,13 @@
+//@media keys
+let menu =document.querySelector('#menu-btn');
+let navbar= document.querySelector('.header .flex .navbar');
+ 
+menu.onclick = () =>
+ {
+     menu.classList.toggle('fa-times');
+     navbar.classList.toggle('active');
+ }
+
+
+
+ 
